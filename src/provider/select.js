c906f8630928be0c31bad2b956981554322c3d87
1582421598 <1582421598@qq.com>
2021-05-05 22:15:23 +0800

    优化匹配(暂行方案)

--- src/provider/select.js
+++ src/provider/select.js
@@ -1,3 +1,27 @@
-module.exports = list => list[0]
+module.exports = (list, info) => {
+	const song = list[0]
+	var judge = false
+	if(info.artists && song.artists.name)
+	{
+		const authorname = song.artists.name
+		info.artists.forEach(artists => 
+		{
+		console.log(artists.name)
+		console.log(authorname)
+		if(!authorname.includes(artists.name))
+		    judge = true
+		});
+		if(judge)
+			return '' // 作者名字不相似就使用下一个音源的结果
+	}
+	if(song.duration)
+	{
+	    if(Math.abs(song.duration - info.duration) < 3 * 1e3)
+			return song // 时长相差3s (3000ms) 之内的结果
+	    else 
+			return '' // 不符合就使用下一个音源的结果
+	}
+	return song
+}
 
 module.exports.ENABLE_FLAC = (process.env.ENABLE_FLAC || '').toLowerCase() === 'true'
