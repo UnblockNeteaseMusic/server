UnblockNeteaseMusic/server

commit 75ef1034176f52063bb0da04ab36ea9866113e78

fix(provider/pyncmd): update api url
Fixes: #522

Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

src/provider/pyncmd.js
@@ -4,7 +4,7 @@ const { getManagedCacheStorage } = require('../cache');
 
 const track = (info) => {
 	const url =
-		'https://service-q3dhb0bw-1302373778.gz.apigw.tencentcs.com/pyncm/track/GetTrackAudio?song_ids=' +
+		'https://service-ghlrryee-1308098780.gz.apigw.tencentcs.com/release/pyncmd/track/GetTrackAudio?song_ids' +
 		info.id +
 		'&bitrate=' +
 		['999000', '320000'].slice(

commit 6754d2faf3cc19127c83397b9eef81e06a647a71

chore(provider/pyncmd): fix typo error
Fixes: 75ef1034176f52 ("fix(provider/pyncmd): update api url")

Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

src/provider/pyncmd.js
@@ -4,7 +4,7 @@ const { getManagedCacheStorage } = require('../cache');
 
 const track = (info) => {
 	const url =
-		'https://service-ghlrryee-1308098780.gz.apigw.tencentcs.com/release/pyncmd/track/GetTrackAudio?song_ids' +
+		'https://service-ghlrryee-1308098780.gz.apigw.tencentcs.com/release/pyncmd/track/GetTrackAudio?song_ids=' +
 		info.id +
 		'&bitrate=' +
 		['999000', '320000'].slice(