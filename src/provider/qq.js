UnblockNeteaseMusic/server

commit b6406308d66aef684c94bd5cd64bd6e1b68154fd

fix(provider/qq): remove deprecated arg in search api
Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

src/provider/qq.js
@@ -20,7 +20,7 @@ const format = (song) => ({
 const search = (info) => {
 	const url =
 		'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?' +
-		'ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center&' +
+		'ct=24&qqmusic_ver=1298&remoteplace=txt.yqq.center&' +
 		't=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w=' +
 		encodeURIComponent(info.keyword) +
 		'&' +

commit 9f7e22e93e87295275c936ef11b63872f5f0a51d

fix(provider/qq): compat with old json data
Fixes: b6406308d66aef ("fix(provider/qq): remove deprecated arg in
 search api")

Fixes: #486
Fixes: #566

Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

src/provider/qq.js
@@ -10,10 +10,10 @@ const headers = {
 };
 
 const format = (song) => ({
-	id: { song: song.mid, file: song.file.media_mid },
-	name: song.name,
+	id: { song: song.songmid, file: song.media_mid },
+	name: song.songname,
 	duration: song.interval * 1000,
-	album: { id: song.album.mid, name: song.album.name },
+	album: { id: song.albummid, name: song.albumname },
 	artists: song.singer.map(({ mid, name }) => ({ id: mid, name })),
 });
 