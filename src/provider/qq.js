b6406308d66aef684c94bd5cd64bd6e1b68154fd
Tianling Shen <i@cnsztl.eu.org>
2022-03-07 15:51:58 +0800

    fix(provider/qq): remove deprecated arg in search api
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- src/provider/qq.js
+++ src/provider/qq.js
@@ -20,7 +20,7 @@ const format = (song) => ({
 const search = (info) => {
 	const url =
 		'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?' +
-		'ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center&' +
+		'ct=24&qqmusic_ver=1298&remoteplace=txt.yqq.center&' +
 		't=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w=' +
 		encodeURIComponent(info.keyword) +
 		'&' +
