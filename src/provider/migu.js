71e3b85f6b80439183df9f0ff42346ed3d8692b1
Tianling Shen <i@cnsztl.eu.org>
2022-04-12 09:50:13 +0800

    feat(provider/migu): migrate to new API
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- src/provider/migu.js
+++ src/provider/migu.js
@@ -3,74 +3,59 @@ const select = require('./select');
 const request = require('../request');
 const { getManagedCacheStorage } = require('../cache');
 
-const headers = {
-	origin: 'http://music.migu.cn/',
-	referer: 'http://m.music.migu.cn/v3/',
-	// cookie: 'migu_music_sid=' + (process.env.MIGU_COOKIE || null),
-	aversionid: process.env.MIGU_COOKIE || null,
-	channel: '0146921',
-};
-
 const format = (song) => {
-	const singerId = song.singerId.split(/\s*,\s*/);
-	const singerName = song.singerName.split(/\s*,\s*/);
 	return {
-		// id: song.copyrightId,
 		id: song.id,
-		name: song.title,
-		album: { id: song.albumId, name: song.albumName },
-		artists: singerId.map((id, index) => ({ id, name: singerName[index] })),
+		name: song.name,
+		album: song.albums && song.albums[0],
+		artists: song.singers,
+		resources: song.newRateFormats.map((detail) => ({
+			formatType: detail.formatType,
+			url: encodeURI(detail.url || detail.androidUrl),
+		})),
 	};
 };
 
 const search = (info) => {
 	const url =
-		'https://m.music.migu.cn/migu/remoting/scr_search_tag?' +
-		'keyword=' +
+		'https://pd.musicapp.migu.cn/MIGUM3.0/v1.0/content/search_all.do?' +
+		'&ua=Android_migu&version=5.0.1&pageNo=1&pageSize=10&text=' +
 		encodeURIComponent(info.keyword) +
-		'&type=2&rows=20&pgc=1';
+		'&searchSwitch=' +
+		'{"song":1,"album":0,"singer":0,"tagSong":0,"mvSong":0,"songlist":0,"bestShow":1}';
 
-	return request('GET', url, headers)
+	return request('GET', url)
 		.then((response) => response.json())
 		.then((jsonBody) => {
-			const list = ((jsonBody || {}).musics || []).map(format);
+			const list = ((jsonBody || {}).songResultData.result || []).map(
+				format
+			);
 			const matched = select(list, info);
-			return matched ? matched.id : Promise.reject();
+			return matched ? matched.resources : Promise.reject();
 		});
 };
 
-const single = (id, format) => {
-	// const url =
-	//	'https://music.migu.cn/v3/api/music/audioPlayer/getPlayInfo?' +
-	//	'dataType=2&' + crypto.miguapi.encryptBody({copyrightId: id.toString(), type: format})
+const single = (resources, format) => {
+	const song = resources.find(
+		(song) => song.url && song.formatType === format
+	);
 
-	const url =
-		'https://app.c.nf.migu.cn/MIGUM2.0/strategy/listen-url/v2.4?' +
-		'netType=01&resourceType=2&songId=' +
-		id.toString() +
-		'&toneFlag=' +
-		format;
-
-	return request('GET', url, headers)
-		.then((response) => response.json())
-		.then((jsonBody) => {
-			// const {playUrl} = jsonBody.data
-			// return playUrl ? encodeURI('http:' + playUrl) : Promise.reject()
-			const { audioFormatType } = jsonBody.data;
-			if (audioFormatType !== format) return Promise.reject();
-			else return url ? jsonBody.data.url : Promise.reject();
-		});
+	if (song) {
+		const playUrl = new URL(song.url);
+		playUrl.protocol = 'http';
+		playUrl.hostname = 'freetyst.nf.migu.cn';
+		return playUrl.href;
+	} else return false;
 };
 
-const track = (id) =>
+const track = (resources) =>
 	Promise.all(
-		// [3, 2, 1].slice(select.ENABLE_FLAC ? 0 : 1)
-		['ZQ24', 'SQ', 'HQ', 'PQ']
+		['ZQ', 'SQ', 'HQ', 'PQ']
 			.slice(select.ENABLE_FLAC ? 0 : 2)
-			.map((format) => single(id, format).catch(() => null))
+			.map((format) => single(resources, format))
 	)
 		.then((result) => result.find((url) => url) || Promise.reject())
-		.catch(() => insure().migu.track(id));
+		.catch(() => insure().migu.track(resources));
 
 const cs = getManagedCacheStorage('provider/migu');
 const check = (info) => cs.cache(info, () => search(info)).then(track);
