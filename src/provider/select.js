d4a615eb3f0eca933aea4854bd984d9adebb414e
pan93412 <pan93412@gmail.com>
2022-05-10 17:19:54 +0800

    refactor(provider/select): clear variable
    We describe what the operation is in the variable name,
    making the code clear to read.

--- src/provider/select.js
+++ src/provider/select.js
@@ -1,12 +1,17 @@
 module.exports = (list, info) => {
 	const { duration, album } = info;
 	const sliceList = list.slice(0, 5); // 挑前5个结果
-	let song = sliceList.find((s) => s.album?.name == album.name); // 第一个专辑名称一样的
-	if (song) return song;
-	song = sliceList.find(
+
+	/* Check if the album is matched */
+	const albumMatchedSong = sliceList.find((s) => s.album?.name == album.name); // 第一个专辑名称一样的
+	if (albumMatchedSong) return albumMatchedSong;
+
+	/* Check if the duration is matched */
+	const durationMatchedSong = sliceList.find(
 		(s) => s.duration && Math.abs(s.duration - duration) < 5 * 1e3
 	); // 第一个时长相差5s (5000ms) 之内的结果
-	if (song) return song;
+	if (durationMatchedSong) return durationMatchedSong;
+
 	else return list[0]; // 没有就播放第一条
 };
 
