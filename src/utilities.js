dac943b5e96b4a9bd2df07083b4d41065d6b69c0
pan93412 <pan93412@gmail.com>
2022-05-10 17:20:54 +0800

    refactor(utilities): add complete docs and types

--- src/utilities.js
+++ src/utilities.js
@@ -10,31 +10,66 @@ const path = require('path');
 const { logScope } = require('./logger');
 const logger = logScope('utilities');
 
-const getConfigData = () => {
+/**
+ * @typedef {{ source: Record<string, string> }} MusicReplaceRecordSource
+ * @typedef {Record<string, unknown>} MusicReplaceRecordEntryField
+ * @typedef {Record<string, MusicReplaceRecordEntryField>} MusicReplaceRecordEntry
+ * @typedef {string} MusicReplaceRecordSourceId
+ * @typedef {Partial<MusicReplaceRecordSource & Record<MusicReplaceRecordSourceId, MusicReplaceRecordEntry>>} MusicUriRecord
+ */
+
+/**
+ * Retrieve the configuration of `musicExample.json`.
+ *
+ * @returns {MusicUriRecord | null}
+ */
+const getConfigData = async () => {
 	try {
 		const fileName = process.env.MUSIC_FILE || 'musicExample.json';
 		const filePath = path.join(__dirname, '.', fileName);
-		let musicDataRaw = fs.readFileSync(filePath);
+		const musicDataRaw = await fs.promises.readFile(filePath);
 		return JSON.parse(musicDataRaw);
 	} catch (error) {
-		logger.error('音源文件加载失败', error.message);
+		logger.warn('Unable to load the musicExample data. Ignoring.', error.message);
 	}
 	return null;
 };
 
+/**
+ * The singleton music configuration data.
+ *
+ * If no `musicExample.json` found or invalid, it will be `{}`.
+ */
 const musicConfigData = getConfigData() || {};
 
+/**
+ * Try to get the matched data from {@link musicConfigData}.
+ *
+ * @param {string} source
+ * @param {string} songId
+ * @returns {MusicReplaceRecordEntryField | null}
+ */
 const tryGetMatchedData = (source, songId) => {
 	let matchedSongData = musicConfigData?.[source]?.[songId];
+
 	if (matchedSongData) {
 		logger.info(
 			matchedSongData,
-			`${songId} Has the matched source (${source})`
+			`${songId} has the matched source (${source})`
 		);
 	}
+
 	return matchedSongData;
 };
 
+/**
+ * Try to get the sources which has the matched record to return.
+ *
+ * @param {string} songId the song ID.
+ * @returns If we found a source that has the match in {@link musicConfigData},
+ * we return `[source]` so `select()` return that data directly; otherwise,
+ * we return `null` to make `select()` to try other sources.
+ */
 const tryGetSelectSource = (songId) => {
 	let source = musicConfigData.source?.[songId];
 	if (source) {
