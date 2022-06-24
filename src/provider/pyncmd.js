5989d0a0b5cdbd784ad490843aa969f9861c28b4
greats3an <greats3an@gmail.com>
2022-06-25 07:59:53 +0800

    fix(provider/pyncmd): migrate api to vercel

--- src/provider/pyncmd.js
+++ src/provider/pyncmd.js
@@ -4,7 +4,7 @@ const { getManagedCacheStorage } = require('../cache');
 
 const track = (info) => {
 	const url =
-		'https://service-ghlrryee-1308098780.gz.apigw.tencentcs.com/release/pyncmd/track/GetTrackAudio?song_ids=' +
+		'https://pyncmd.vercel.app/api/pyncm?module=track&method=GetTrackAudio&song_ids=' +
 		info.id +
 		'&bitrate=' +
 		['999000', '320000'].slice(
