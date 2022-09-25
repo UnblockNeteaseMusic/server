UnblockNeteaseMusic/server

commit 10103d53453897ed66fc8ae6abdee922f7e0ddb9

fix(hook): rework with latest PC clients
Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

src/hook.js
@@ -62,6 +62,7 @@ hook.target.path = new Set([
 	'/api/song/enhance/player/url',
 	'/api/song/enhance/player/url/v1',
 	'/api/song/enhance/download/url',
+	'/api/song/enhance/download/url/v1',
 	'/api/song/enhance/privilege',
 	'/batch',
 	'/api/batch',
@@ -167,7 +168,10 @@ hook.request.before = (ctx) => {
 					ctx.netease = netease;
 					// console.log(netease.path, netease.param)
 
-					if (netease.path === '/api/song/enhance/download/url')
+					if (
+						netease.path === '/api/song/enhance/download/url' ||
+						netease.path === '/api/song/enhance/download/url/v1'
+					)
 						return pretendPlay(ctx);
 				}
 			})
@@ -338,6 +342,19 @@ hook.request.after = (ctx) => {
 							value['unplayableType'] = 'unknown';
 							value['unplayableUserIds'] = [];
 						}
+						if ('noCopyrightRcmd' in value)
+							value['noCopyrightRcmd'] = null;
+						if (
+							'payed' in value &&
+							value['flLevel'] === 'none' &&
+							value['plLevel'] === 'none' &&
+							value['dlLevel'] === 'none'
+						) {
+							value['flLevel'] = 'exhigh';
+							value['plLevel'] = 'exhigh';
+							value['dlLevel'] = 'exhigh';
+							value['payed'] = 1;
+						}
 					}
 					return value;
 				};
@@ -412,7 +429,21 @@ const pretendPlay = (ctx) => {
 		netease.param = { ids: `["${id}"]`, br };
 		query = crypto.linuxapi.encryptRequest(turn, netease.param);
 	} else {
-		const { id, br, e_r, header } = netease.param;
+		let { id, br, level, e_r, header } = netease.param;
+		if (!br && level) {
+			switch (level) {
+				case 'hires':
+				case 'loseless':
+					br = 999000;
+					break;
+				case 'exhigh':
+					br = 320000;
+					break;
+				case 'standard':
+					br = 128000;
+					break;
+			}
+		}
 		netease.param = { ids: `["${id}"]`, br, e_r, header };
 		query = crypto.eapi.encryptRequest(turn, netease.param);
 	}

commit 94bdf2488da9ff550cd336a745110209aa683657

chore(src/hook): fix typo error
`loseless` -> `lossless`

Fixes: 10103d53453897 ("fix(hook): rework with latest PC clients")

Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

precompiled/app.js
@@ -109,7 +109,7 @@ const pretendPlay=ctx=>{const{req,netease}=ctx,turn="http://music.163.com/api/so
 let query
 if(netease.forward){const{id,br}=netease.param
 netease.param={ids:`["${id}"]`,br},query=crypto.linuxapi.encryptRequest(turn,netease.param)}else{let{id:id1,br:br1,level,e_r,header}=netease.param
-if(!br1&&level)switch(level){case"hires":case"loseless":br1=999e3
+if(!br1&&level)switch(level){case"hires":case"lossless":br1=999e3
 break
 case"exhigh":br1=32e4
 break

src/hook.js
@@ -433,7 +433,7 @@ const pretendPlay = (ctx) => {
 		if (!br && level) {
 			switch (level) {
 				case 'hires':
-				case 'loseless':
+				case 'lossless':
 					br = 999000;
 					break;
 				case 'exhigh':