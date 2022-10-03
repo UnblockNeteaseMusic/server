90badd1fe19d19b29c0d3c397fe7e5909f046809
GY <dev.youngkwok718@gmail.com>
2022-10-03 12:42:43 +0800

    build: reset port

--- nw.js
+++ nw.js
@@ -4,7 +4,7 @@ const svc = new Service({
 	name: 'unblock-netease-cloud-music',
 	description: '点亮网易云音乐灰色歌曲',
 	script: './app.js', // 入口文件路径
-	scriptOptions: '-p 52100', // 可选参数
+	// scriptOptions: '-p 52100', // 可选参数
 	wait: '1', // 程序崩溃后重启时间间隔
 	grow: '0.25', // 重启等待时间成长值，第一次1秒，第二次1.25秒。。。
 	maxRestarts: '40', // 60秒内最大重启次数
