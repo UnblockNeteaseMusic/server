9fb9b8ea8e1185d1963bde038edac138b9279d0d
Tianling Shen <i@cnsztl.eu.org>
2023-01-09 18:43:55 +0800

    feat(provider/qq): check if the music link is accessible
    Fallback to worse audio encoding quality if QQ rejected the
    request (with 404 response).
    
    Fixes: #942
    
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- src/provider/qq.js
+++ src/provider/qq.js
@@ -57,7 +57,7 @@ const single = (id, format) => {
 					param: {
 						guid: (Math.random() * 10000000).toFixed(0),
 						loginflag: 1,
-						filename: [format.join(id.file)],
+						filename: format[0] ? [format.join(id.file)] : null,
 						songmid: [id.song],
 						songtype: [0],
 						uin,
@@ -71,9 +71,18 @@ const single = (id, format) => {
 		.then((response) => response.json())
 		.then((jsonBody) => {
 			const { sip, midurlinfo } = jsonBody.req_0.data;
-			return midurlinfo[0].purl
-				? sip[0] + midurlinfo[0].purl
-				: Promise.reject();
+			if (!midurlinfo[0].purl) return Promise.reject();
+
+			const playurl = sip[0] + midurlinfo[0].purl;
+			const header = {
+				range: 'bytes=0-8191',
+				'accept-encoding': 'identity',
+			};
+			return request('GET', playurl, header).then((response) => {
+				if (response.statusCode < 200 || response.statusCode > 299)
+					return Promise.reject();
+				else return playurl;
+			});
 		});
 };
 
@@ -84,6 +93,7 @@ const track = (id) => {
 			['F000', '.flac'],
 			['M800', '.mp3'],
 			['M500', '.mp3'],
+			[null, null],
 		]
 			.slice(
 				headers.cookie || typeof window !== 'undefined'
