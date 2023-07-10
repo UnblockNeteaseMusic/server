6f95f53f1fc9f177e72e0d6b2c6ea36344e4ea6d
pan93412 <pan93412@gmail.com>
2023-07-11 02:38:40 +0800

    feat: Implement dead-simple dotenv for bootstrap

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -1,14 +1,17 @@
-const dotenv = require('dotenv');
+// @ts-check
+
+const { loadDotenv } = require('../dotenv.js');
 const devMessage = require('./message');
 
 /**
  * Start the main part.
  *
  * @param {string} mainEntry
+ * @returns {Promise<void>}
  */
-function startApp(mainEntry) {
+async function startApp(mainEntry) {
 	// Inject the `.env` file into the `process.env` object.
-	dotenv.config();
+	await loadDotenv();
 
 	if (process.env.DEVELOPMENT === 'true') {
 		console.warn(devMessage);
