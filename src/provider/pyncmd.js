3ab82aad73173807d5c99e98c2c3f792059f5360
LittleRey <34397448+LittleRey@users.noreply.github.com>
2023-07-23 10:17:08 +0800

    Update pyncmd.js

--- src/provider/pyncmd.js
+++ src/provider/pyncmd.js
@@ -4,7 +4,7 @@ const { getManagedCacheStorage } = require('../cache');
 
 const track = (info) => {
 	const url =
-		'https://netease.buildtest.site/api/pyncm?module=track&method=GetTrackAudio&song_ids=' +
+		'https://pyncmd.vercel.app/api/pyncm?module=track&method=GetTrackAudio&song_ids=' +
 		info.id +
 		'&bitrate=' +
 		['999000', '320000'].slice(
@@ -12,7 +12,7 @@ const track = (info) => {
 			select.ENABLE_FLAC ? 1 : 2
 		);
 	const headers = {
-		Host: 'music.163-my-beloved.com',
+		Host: 'pyncmd.vercel.app',
 	};
 	return request('GET', url, headers)
 		.then((response) => response.json())
