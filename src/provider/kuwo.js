c2b17924102a0964332ca55f82e34b87be596440
Inkarro <137393706+Inkarro@users.noreply.github.com>
2023-08-17 21:31:35 +0800

    Update src/provider/kuwo.js
    Co-authored-by: Tianling Shen <cnsztl@gmail.com>

--- src/provider/kuwo.js
+++ src/provider/kuwo.js
@@ -8,7 +8,7 @@ const format = (song) => ({
 	id: song.MUSICRID.split('_').pop(),
 	name: song.SONGNAME,
 	// duration: song.songTimeMinutes.split(':').reduce((minute, second) => minute * 60 + parseFloat(second), 0) * 1000,
-	duration: song.duration * 1000,
+	duration: song.DURATION * 1000,
 	album: { id: song.ALBUMID, name: song.ALBUM },
 	artists: song.ARTIST
 		.split('&')
