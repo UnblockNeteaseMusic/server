7097918d91ad2344f09068d5ac6a84dd55b68651
Tianling Shen <i@cnsztl.eu.org>
2024-02-07 20:12:39 +0800

    feat(request): update user-agent to recent version
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- src/request.js
+++ src/request.js
@@ -95,7 +95,7 @@ const request = (
 			'accept-encoding': 'gzip, deflate',
 			'accept-language': 'zh-CN,zh;q=0.9',
 			'user-agent':
-				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
 			...headers,
 		},
 		proxy
