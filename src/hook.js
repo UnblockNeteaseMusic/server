008b006dc87d11f7cf19567ca84ee94b2effb47a
Inkarro <137393706+Inkarro@users.noreply.github.com>
2025-11-04 20:38:59 +0800

    fix: 适配新版客户端，修复 nativeFetch 导致的请求参数错误

--- src/hook.js
+++ src/hook.js
@@ -45,6 +45,7 @@ hook.target.host = new Set([
 	'music.163.com',
 	'interface.music.163.com',
 	'interface3.music.163.com',
+	'interfacepc.music.163.com',
 	'apm.music.163.com',
 	'apm3.music.163.com',
 	'interface.music.163.com.163jiasu.com',
@@ -169,7 +170,8 @@ hook.request.before = (ctx) => {
 					req.url.includes('/eapi/cloud/upload/check')
 				)
 					return; // look living/cloudupload eapi can not be decrypted
-				req.headers['Accept-Encoding'] = 'gzip, deflate'; // https://blog.csdn.net/u013022222/article/details/51707352
+				if (req.headers['sec-ch-ua'])
+					req.headers['Accept-Encoding'] = 'gzip, deflate'; // https://blog.csdn.net/u013022222/article/details/51707352
 				if (body) {
 					const netease = {};
 					netease.pad = (body.match(/%0+$/) || [''])[0];
